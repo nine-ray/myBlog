2079e34e2a44b2eefa66ac2f5f6aec4b7b45773d
韩哓红 <XiaoHong.Han@five-star.cn>
2020-04-21 17:44:37 +0800

    页面布局

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,14 @@
-import React, {PureComponent} from 'react';
-import {Avatar, Card, Col, Input, Popover, Row, Select, Tag} from 'antd';
-import {DownOutlined} from '@ant-design/icons';
-import {baidu, bdIcon} from "../assets/image/images"
-import {connect} from 'dva';
+import React, { PureComponent } from 'react';
+import { Avatar, Card, Col, Input, Popover, Row, Select, Tag, Icon, Modal, Form } from 'antd';
+// import {DownOutlined} from '@ant-design/icons';
+import { baidu, bdIcon } from "../assets/image/images"
+import { connect } from 'dva';
 
 
-const {Search} = Input;
-const {CheckableTag} = Tag;
-const {Option} = Select;
-const {Meta} = Card;
+const { Search } = Input;
+const { CheckableTag } = Tag;
+const { Option } = Select;
+const { Meta } = Card;
 
 let deskHeight = 400;
 let deskDivWidth = 1440;
@@ -18,10 +18,14 @@ let deskDivWidth = 1440;
     deskDivWidth: common.deskDivWidth
 }))
 class SearchBar extends PureComponent {
-    state = {
-        currentTool: 0,
-        currentKey: ""
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentTool: 0,
+            currentKey: "",
+            visible: false,
+        };
+    }
 
     searchToOther = (keyword) => {
         const currentTool = this.state.currentTool;
@@ -57,62 +61,127 @@ class SearchBar extends PureComponent {
         deskDivWidth = nextProps.deskDivWidth;
         this.forceUpdate();
     }
-
+    // 生成唯一标示
+    guid() {
+        function S4() {
+          return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+        }
+        return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+      }
     render() {
-        const handleMenuClick = (e) => {
-            console.log("e", e);
-        };
+        const { getFieldDecorator, validateFieldsAndScroll, resetFields } = this.props.form
         const content = (
-            <Row gutter={[24, 24]} justify="space-around" style={{width: deskDivWidth * 0.4}}>
+            <Row gutter={[24, 24]} justify="space-around" style={{ width: 576 }}>
+                {/* deskDivWidth * 0.4, */}
                 {this.toolList.map((item) => {
                     return <Col key={item.description}
-                                style={{backgroundColor: "#f9f9f9", height: 30, padding: 2, margin: 15}} span={4}>
-                        <Avatar src={item.icon} size={20}/>
+                        style={{ backgroundColor: "#f9f9f9", height: 30, padding: 2, margin: 15 }} span={4}>
+                        <Avatar src={item.icon} size={20} />
                         <span>{item.name}</span>
                     </Col>
                 })}
             </Row>
         );
-
+        // 添加文件夹modal---关闭
+        const handleCancel = () => {
+            this.setState({
+                visible: false
+            })
+            resetFields()
+        }
+        // 添加文件夹modal---确定
+        const handleOk = () => {
+            validateFieldsAndScroll((errors, values) => {
+                if (errors) {
+                    return null
+                } else {
+                    let dataInfo = localStorage.getItem("foldData")? JSON.parse(localStorage.getItem("foldData")):[]
+                    console.log('dataInfo-------', dataInfo)
+                    dataInfo.push({
+                        topic: values.folderName,
+                        type: 'folder',
+                        group: [],
+                        uuid: this.guid()
+                    })
+                    console.log('为了生成唯一标示-----', dataInfo)
+                    localStorage.setItem('foldData',JSON.stringify(dataInfo))
+                    resetFields()
+                    this.setState({
+                        visible: false
+                    })
+                }
+            });
+        }
+        // 添加文件夹
+        const handleAddFolder = () => {
+            this.setState({
+                visible: true
+            })
+        }
+        const layout = {
+            labelCol: { span: 7 },
+            wrapperCol: { span: 16 },
+        }
         return (
-            <div style={{width: "100%", height: 50, textAlign: "center"}}>
+            <div style={{ width: "100%", height: 50, textAlign: "center" }}>
                 <Popover content={content} placement="bottomLeft">
                     <div className="ant-dropdown-link"
-                         style={{height: "100%", display: "inline-block", position: "relative", bottom: 50}}
-                         onClick={e => e.preventDefault()}>
-                        <img style={{width: 80, height: "100%"}} src={baidu}/>
-                        <DownOutlined style={{position: "relative", top: 15}}/>
+                        style={{ height: "100%", display: "inline-block", position: "relative", bottom: 50 }}
+                        onClick={e => e.preventDefault()}>
+                        <img style={{ width: 80, height: "100%" }} src={baidu} />
+                        <Icon type="down" style={{ position: "relative", top: 15 }} />
+                        {/* <DownOutlined /> */}
                     </div>
                 </Popover>
-                <div style={{display: "inline-block", width: "60%", paddingLeft: 0}}>
+                <div style={{ display: "inline-block", width: "60%", paddingLeft: 0 }}>
                     <Search
                         placeholder="填写搜索内容"
                         onSearch={value => this.searchToOther(value)}
                         size="large"
                         style={{
-                            width: "100%",
+                            width: "90%",
                         }}
                     />
+                    <a style={{ width: '10%', marginLeft: '10px' }}><Icon onClick={() => { handleAddFolder() }} style={{ fontSize: '30px' }} type="plus-square" /></a>
                     <Row justify="start"
-                         style={{
-                             width: deskDivWidth * 0.6,
-                             marginInline: "auto",
-                             display: "inline-block",
-                             marginTop: 5
-                         }}>
+                        style={{
+                            width: 864, // deskDivWidth * 0.6, 
+                            marginInline: "auto",
+                            display: "inline-block",
+                            marginTop: 5
+                        }}>
                         {this.recommendList.map((item) => {
                             return <Col span={2} key={item.link}>
                                 <CheckableTag color={item.color}
-                                              onChange={(e) => {
-                                                  this.skipToOther(item.link)
-                                              }}>{item.name}</CheckableTag>
+                                    onChange={(e) => {
+                                        this.skipToOther(item.link)
+                                    }}>{item.name}</CheckableTag>
                             </Col>
                         })}
                     </Row>
                 </div>
+                {/* 添加文件夹名称modal */}
+                <Modal
+                    title="添加文件夹"
+                    onCancel={handleCancel}
+                    onOk={handleOk}
+                    visible={this.state.visible}
+                >
+                    <Form
+                        {...layout}
+                    >
+                        <Form.Item
+                            label="文件夹名称"
+                            required={true}
+                        >
+                            {getFieldDecorator('folderName')(<Input />)}
+                        </Form.Item>
+                    </Form>
+
+                </Modal>
             </div>
         );
     }
 }
 
-export default SearchBar;
+export default Form.create()(SearchBar);
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,20 +1,16 @@
-import styles from './index.css';
-import React, {PureComponent} from 'react';
-import {Avatar, Card, Form, Input, Modal, Select} from 'antd';
+import styles from './index.less';
+import React, { PureComponent } from 'react';
+import { Avatar, Card, Form, Input, Modal, Select, Button, Menu, Dropdown, Icon, Tag, Tooltip } from 'antd';
 import SearchBar from "../../components/SearchBar"
-import {connect} from 'dva';
-import {CloseOutlined, PlusCircleOutlined} from '@ant-design/icons';
+import { connect } from 'dva';
+import { CloseOutlined, PlusCircleOutlined } from '@ant-design/icons';
 
-import {lind2Icon, skipToOther} from "../../util/utils"
+import { lind2Icon, skipToOther } from "../../util/utils"
 
-
-const {Search} = Input;
-const {Option} = Select;
-const {Meta, Grid} = Card;
-const {Item} = Form;
+const { Item } = Form;
 const layout = {
-    labelCol: {span: 8},
-    wrapperCol: {span: 16},
+    labelCol: { span: 4 },
+    wrapperCol: { span: 16 },
 };
 
 
@@ -24,133 +20,332 @@ const layout = {
 }))
 @Form.create()
 class Home extends PureComponent {
-    state = {
-        currentTool: 0,
-        currentKey: ""
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false,
+            uuid: ''
+        };
+    }
 
     componentWillMount() {
-        const strs = localStorage.getItem("collect");
-        this.collectList = JSON.parse(strs);
+        let dataInfo = localStorage.getItem("foldData") ? JSON.parse(localStorage.getItem("foldData")) : []
+        this.setState({
+            dataInfo: dataInfo || []
+        })
     }
 
-    collectList = [];
+    // collectListSort = () => {
+    //     this.collectList.sort((a, b) => b.collectOrder - a.collectOrder)
+    // };
 
-    componentDidMount() {
-
-    }
-
-    collectListSort = () => {
-        this.collectList.sort((a, b) => b.collectOrder - a.collectOrder)
-    };
-    deleteCollectList = (collectId) => {
-        this.collectList = this.collectList.filter(item => item.collectId !== collectId)
-        this.collectListSort();
-        localStorage.clear();
-        localStorage.setItem("collect", JSON.stringify(this.collectList));
-        this.forceUpdate();
-    }
-
-    changeCollectList = (collect) => {
-        const index = this.collectList.findIndex((value) => {
-            if (value.collectId === collect.collectId) {
-                return true;
-            }
-            return false;
-        });
-        if (index >= 0) {
-            this.collectList.map((item, i) => {
-                if (i !== index && i <= 5) {
-                    item.collectOrder = item.collectOrder - 1;
-                } else if (i === index) {
-                    item = collect
-                }
-                return item;
-            });
-            this.collectList = this.collectList.filter(item => item.collectOrder > 0)
-        } else {
-            this.collectList.push(collect)
-        }
-        this.collectListSort();
-        localStorage.clear();
-        localStorage.setItem("collect", JSON.stringify(this.collectList));
-        this.forceUpdate();
-    };
-    showModal = () => {
+    // changeCollectList = (collect) => {
+    //     const index = this.collectList.findIndex((value) => {
+    //         if (value.collectId === collect.collectId) {
+    //             return true;
+    //         }
+    //         return false;
+    //     });
+    //     if (index >= 0) {
+    //         this.collectList.map((item, i) => {
+    //             if (i !== index && i <= 5) {
+    //                 item.collectOrder = item.collectOrder - 1;
+    //             } else if (i === index) {
+    //                 item = collect
+    //             }
+    //             return item;
+    //         });
+    //         this.collectList = this.collectList.filter(item => item.collectOrder > 0)
+    //     } else {
+    //         this.collectList.push(collect)
+    //     }
+    //     this.collectListSort();
+    //     localStorage.clear();
+    //     localStorage.setItem("collect", JSON.stringify(this.collectList));
+    //     this.forceUpdate();
+    // };
+    // 显示添加urlmodal
+    showModal = (uuid) => {
         this.setState({
             visible: true,
+            uuid
         });
     };
-    handleCancel = e => {
-        console.log(e);
+    // 添加urlmodal---取消
+    handleCancel = () => {
         this.setState({
             visible: false,
+            uuid: ''
         });
+        this.props.form.resetFields()
     };
+    // 删除url
+    handleDelete = (eleUuid, itemUuid) => {
+        const dataInfoMid = this.state.dataInfo.map(ele => {
+            if (ele.uuid === eleUuid) {
+                let groupMid=ele.group.filter(item=>{if(item.uuid!==itemUuid)return item})
+                return ({
+                    ...ele,
+                    group: [...groupMid]
+                })
+            } else {
+                return ({ ...ele })
+            }
+        })
+        localStorage.setItem('foldData', JSON.stringify(dataInfoMid))
+    }
+    // 显示页面
+    handleShow = (url) => {
+        window.open(url)
+    }
+    // card中的url
+    handleUrl = (ele) => {
+        const buttonComs = []
+        ele.group.forEach((item, index) => {
+            if (item.type === 'file') {
+                buttonComs.push(
+                    <div style={{ width: '55px', height: '20px', marginRight: '4px', display: 'flex', flexDirection: 'row', flexWrap: 'nowrap', backgroundColor: '#DDDDDD', marginBottom: '5px' }}>
+                        <Tooltip title={item.name}>
+                            <div style={{ width: '80%' }}><a style={{ width: '100%' }} onClick={() => { this.handleShow(item.url) }}>{item.name && item.name.length > 2 ? `${item.name.substring(0, 2)}...` : item.name || ''}</a></div>
+                        </Tooltip>
+                        <div style={{ width: '20%' }}><a style={{ color: 'red' }} onClick={() => { this.handleDelete(ele.uuid, item.uuid) }}>x</a></div>
+                    </div>
+                )
+            }
+        })
+        return buttonComs
+    }
+    // 每个card
+    handleRender = (arrayDatas) => {
+        const renderCom = []
+        if (arrayDatas.length) {
+            arrayDatas.forEach((item, index) => {
+                if (item.type === 'folder') {
+                    renderCom.push(
+                        <Card title={item.topic} className={styles.itemCard} extra={<a onClick={() => { this.showModal(item.uuid) }}>添加</a>} style={{ height: '200px' }}>
+                            <div style={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start' }}>
+                                {item.group && item.group.length ? this.handleUrl(item) : []}
+                            </div>
+                        </Card>
+                    )
+                }
+            })
 
-
+        }
+        return (renderCom)
+    }
+    // 生成唯一标示
+    guid() {
+        function S4() {
+            return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+        }
+        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+    }
     render() {
         const deskDivWidth = this.props.deskDivWidth || 1440
         const {
             form
         } = this.props;
-
-        const {getFieldDecorator} = form
-
-        const handleOk = e => {
+        let { dataInfo } = this.state
+        const { getFieldDecorator, resetFields } = form
+        // 添加urlmodal----确定
+        const handleOk = () => {
             form.validateFields((err, fieldsValue) => {
                 if (err) return;
-                const {link, name} = fieldsValue;
-                this.changeCollectList({
-                    collectId: new Date().getMilliseconds(),
-                    collectOrder: 100,
-                    link,
-                    name
+                const { link, name } = fieldsValue;
+                const dataInfoMid = dataInfo.map(item => {
+                    if (item.uuid === this.state.uuid) {
+                        let groupMid = []
+                        if (item.group && item.group instanceof Array) {
+                            groupMid = [...item.group]
+                            groupMid.push({
+                                type: 'file',
+                                name: name,
+                                url: link,
+                                uuid: this.guid()
+                            })
+                        }
+                        else {
+                            groupMid.push({
+                                type: 'file',
+                                name: name,
+                                url: link,
+                                uuid: this.guid()
+                            })
+                        }
+                        return ({
+                            ...item,
+                            group: [...groupMid]
+                        })
+                    } else {
+                        return ({ ...item })
+                    }
                 })
+                localStorage.setItem('foldData', JSON.stringify(dataInfoMid))
+                resetFields()
+                this.setState({
+                    uuid: ''
+                })
+                // this.changeCollectList({
+                //     collectId: new Date().getMilliseconds(),
+                //     collectOrder: 100,
+                //     link,
+                //     name
+                // })
             });
             this.setState({
                 visible: false,
             });
         };
-        const gridType = {
-            width: deskDivWidth / 12,
-            height: deskDivWidth / 12,
-            textAlign: 'center',
-            margin: 5,
-            padding: 30
-        }
+        // const gridType = {
+        //     width: deskDivWidth / 12,
+        //     height: deskDivWidth / 12,
+        //     textAlign: 'center',
+        //     margin: 5,
+        //     padding: 30
+        // }
+        const arrayData = [
+            {
+                topic: '社交',
+                type: 'folder',
+                group: [{
+                    type: 'file',
+                    name: '邮箱，邮箱邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }]
+            }, {
+                topic: '社交',
+                type: 'folder',
+                group: [{
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }]
+            }, {
+                topic: '社交',
+                type: 'folder',
+                group: [{
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                },]
+            }, {
+                topic: '社交',
+                type: 'folder',
+                group: [{
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                },]
+            }, {
+                topic: '社交',
+                type: 'folder',
+                group: [{
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }]
+            }, {
+                topic: '社交',
+                type: 'folder',
+                group: [{
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }, {
+                    type: 'file',
+                    name: '邮箱',
+                    url: 'https://mail.163.com/',
+                }]
+            },
+        ]
         return (
             <div className={styles.normal}>
-                <SearchBar/>
-                <Card bordered={false} style={{width: "60%", marginTop: 70, height: "70%"}}>
-                    {this.collectList.map((item, i) => {
-                        if (i >= 11) {
-                            return null;
-                        }
-                        const icon = lind2Icon(item.link);
-                        console.log("icon", icon);
-                        return <Grid key={item.collectId} style={gridType} hoverable={true} onClick={(e) => {
-                            item.collectOrder = item.collectOrder + 1;
-                            this.changeCollectList(item);
-                            skipToOther(item.link)
-                        }}>
-                            <CloseOutlined onClick={(e) => {
-                                this.deleteCollectList(item.collectId);
-                                e.stopPropagation();
-                            }} style={{
-                                position: "relative",
-                                bottom: gridType.width * 0.3,
-                                left: gridType.width * 0.55
-                            }}/>
-                            <Avatar src={icon}>无</Avatar>
-                            <p style={{margin: 10}}>{item.name}</p>
-                        </Grid>
-                    })}
-                    <Grid key={-1} style={gridType} hoverable={true} onClick={this.showModal}>
-                        <PlusCircleOutlined style={{fontSize: gridType.width / 2, color: "#f0f2f5"}}/>
-                    </Grid>
-                </Card>
-
+                <SearchBar />
+                {/* 中间内容 */}
+                <div className={styles.centerContent}>
+                    {this.handleRender(dataInfo && dataInfo.length ? dataInfo : [])}
+                </div>
                 <Modal
                     title="快捷链接管理"
                     onCancel={this.handleCancel}
@@ -165,13 +360,13 @@ class Home extends PureComponent {
                             required={true}
                             placeholder="链接地址，以https或者http起头"
                         >
-                            {getFieldDecorator('link')(<Input placeholder="链接地址，以https或者http起头"/>)}
+                            {getFieldDecorator('link')(<Input placeholder="链接地址，以https或者http起头" />)}
                         </Item>
                         <Item
                             label="链接名称"
                             name="name"
                         >
-                            {getFieldDecorator('name')(<Input placeholder="填写链接名称"/>)}
+                            {getFieldDecorator('name')(<Input placeholder="填写链接名称" />)}
 
                         </Item>
                     </Form>
